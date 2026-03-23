7025bae0b1b321290aec636849ec2925dd6dd3dd
Mayur Tarate <133866255+mayur200904@users.noreply.github.com>
2026-03-23 14:25:12 +0530

    chore(tina): update generated client artifacts

--- woc-starter-v2/tina/__generated__/client.ts
+++ woc-starter-v2/tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '6e122eefc0648b97748519ce4b6c8b678283185d', queries,  });
+export const client = createClient({ cacheDir: '/Users/mayursantoshtarate/Desktop/project/IKF/ikf-woc-starter-pack/woc-starter-v2/tina/__generated__/.cache/1774250086838', url: 'https://content.tinajs.io/2.2/content/dcf16d36-5bf7-453a-a56c-0f51f4d2192b/github/main', token: '6e122eefc0648b97748519ce4b6c8b678283185d', queries,  });
 export default client;
   
--- woc-starter-v2/tina/__generated__/types.ts
+++ woc-starter-v2/tina/__generated__/types.ts
@@ -1605,7 +1605,7 @@ export const ExperimentalGetTinaClient = () =>
   getSdk(
     generateRequester(
       createClient({
-        url: "http://localhost:4001/graphql",
+        url: "https://content.tinajs.io/2.2/content/dcf16d36-5bf7-453a-a56c-0f51f4d2192b/github/main",
         queries,
       })
     )
